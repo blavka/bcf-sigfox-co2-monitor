a9b20cab01e2d67abb6442e1306b08edc2d24efe
Pavel Hübner <pavel.hubner@bigclown.com>
2017-04-18 16:34:43 +0200

    Enable SigFox driver, disable CO2, fix pressure format

--- app/application.c
+++ app/application.c
@@ -4,7 +4,6 @@
 #define FIRST_REPORT_SECONDS 60
 #define REGULAR_REPORT_SECONDS (15 * 60)
 
-
 struct
 {
     struct { bool valid; float value; } temperature;
@@ -94,7 +93,7 @@ void timer_task(void *param)
 
 	if (sensor.pressure.valid)
 	{
-        header |= 0x04; pressure = sensor.pressure.value;
+        header |= 0x04; pressure = sensor.pressure.value / 2;
 	}
 
     if (sensor.co2_concentration.valid)
@@ -132,12 +131,12 @@ void application_init(void)
 
     bc_module_battery_init(BC_MODULE_BATTERY_FORMAT_STANDARD);
 
-    //bc_module_sigfox_init(&sigfox_module, BC_MODULE_SIGFOX_REVISION_R2);
-	//bc_module_sigfox_set_event_handler(&sigfox_module, sigfox_module_event_handler, NULL);
+    bc_module_sigfox_init(&sigfox_module, BC_MODULE_SIGFOX_REVISION_R2);
+    bc_module_sigfox_set_event_handler(&sigfox_module, sigfox_module_event_handler, NULL);
 
-    bc_module_co2_init();
-    bc_module_co2_set_update_interval(SENSOR_UPDATE_INTERVAL_SECONDS * 1000);
-    bc_module_co2_set_event_handler(co2_module_event_handler, NULL);
+    //bc_module_co2_init();
+    //bc_module_co2_set_update_interval(SENSOR_UPDATE_INTERVAL_SECONDS * 1000);
+    //bc_module_co2_set_event_handler(co2_module_event_handler, NULL);
 
     bc_tag_temperature_init(&temperature_tag_internal, BC_I2C_I2C0, BC_TAG_TEMPERATURE_I2C_ADDRESS_ALTERNATE);
     bc_tag_temperature_set_update_interval(&temperature_tag_internal, SENSOR_UPDATE_INTERVAL_SECONDS * 1000);
@@ -157,5 +156,5 @@ void application_init(void)
 
     bc_led_set_mode(&led, BC_LED_MODE_OFF);
 
-    //bc_scheduler_register(timer_task, NULL, FIRST_REPORT_SECONDS * 1000);
+    bc_scheduler_register(timer_task, NULL, FIRST_REPORT_SECONDS * 1000);
 }
